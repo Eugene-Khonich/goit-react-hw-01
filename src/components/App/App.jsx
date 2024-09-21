067764e82e56165d02842cbf91f66fdecc940ac3
Eugene-Khonich <ehonich@gmail.com>
2024-09-21 23:38:54 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,19 @@
 import './App.module.css';
+import userData from '../../userData.json';
+import Profile from '../Profile/Profile';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
